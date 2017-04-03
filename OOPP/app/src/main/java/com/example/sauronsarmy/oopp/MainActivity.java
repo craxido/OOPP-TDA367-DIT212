8dc291bb2425f67fa944b43c509ab66011fb1f5f
Erik Sjöström <erikdsjostrom@gmail.com>
2017-04-03 22:22:58 +0200

    Moved clickListener logic outside of the onCreate function

--- OOPP/app/src/main/java/com/example/sauronsarmy/oopp/MainActivity.java
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/MainActivity.java
@@ -8,10 +8,6 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageButton homeButton;
-    private ImageButton mapButton;
-    private ImageButton shopButton;
-    private ImageButton statsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,36 +17,36 @@ public class MainActivity extends AppCompatActivity {
         Clicking on Home/Shop/Map/Stats should send the user to the
         appropriate activity.
          */
-        homeButton  = (ImageButton) findViewById(R.id.b_home);
-        mapButton   = (ImageButton) findViewById(R.id.b_map);
-        statsButton = (ImageButton) findViewById(R.id.b_stats);
-        shopButton  = (ImageButton) findViewById(R.id.b_shop);
+        ImageButton homeButton  = (ImageButton) findViewById(R.id.b_home);
+        ImageButton mapButton   = (ImageButton) findViewById(R.id.b_map);
+        ImageButton statsButton = (ImageButton) findViewById(R.id.b_stats);
+        ImageButton shopButton  = (ImageButton) findViewById(R.id.b_shop);
 
-        View.OnClickListener buttonListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               Context context = MainActivity.this;
-                // Figure out which button was pressed
-                switch (v.getId()) {
-                    case R.id.b_home:
-                        startActivity(new Intent(context, HomeActivity.class));
-                        break;
-                    case R.id.b_map:
-                        startActivity(new Intent(context, MapActivity.class));
-                        break;
-                    case R.id.b_shop:
-                        startActivity(new Intent(context, ShopActivity.class));
-                        break;
-                    case R.id.b_stats:
-                        startActivity(new Intent(context, StatsActivity.class));
-                        break;
-                }
-            }
-        };
         homeButton.setOnClickListener(buttonListener);
         shopButton.setOnClickListener(buttonListener);
         mapButton.setOnClickListener(buttonListener);
         statsButton.setOnClickListener(buttonListener);
     }
 
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Context context = MainActivity.this;
+            // Figure out which button was pressed
+            switch (v.getId()) {
+                case R.id.b_home:
+                    startActivity(new Intent(context, HomeActivity.class));
+                    break;
+                case R.id.b_map:
+                    startActivity(new Intent(context, MapActivity.class));
+                    break;
+                case R.id.b_shop:
+                    startActivity(new Intent(context, ShopActivity.class));
+                    break;
+                case R.id.b_stats:
+                    startActivity(new Intent(context, StatsActivity.class));
+                    break;
+            }
+        }
+    };
 }
