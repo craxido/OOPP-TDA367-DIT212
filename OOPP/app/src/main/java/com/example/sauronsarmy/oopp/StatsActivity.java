7bc2577ca59bd8ce3c2241af6806e7d57e07de81
Erik Sjöström <erikdsjostrom@gmail.com>
2017-04-03 22:58:52 +0200

    Copy pasted some code with miner alterations, too allow every activity to reach all other activities

--- OOPP/app/src/main/java/com/example/sauronsarmy/oopp/StatsActivity.java
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/StatsActivity.java
@@ -1,7 +1,11 @@
 package com.example.sauronsarmy.oopp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -9,5 +13,44 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        /*
+        Clicking on Home/Shop/Map/Stats should send the user to the
+        appropriate activity.
+         */
+        ImageButton homeButton  = (ImageButton) findViewById(R.id.b_home);
+        ImageButton mapButton   = (ImageButton) findViewById(R.id.b_map);
+        ImageButton statsButton = (ImageButton) findViewById(R.id.b_stats);
+        ImageButton shopButton  = (ImageButton) findViewById(R.id.b_shop);
+        ImageButton mainButton  = (ImageButton) findViewById(R.id.b_main);
+
+        homeButton.setOnClickListener(buttonListener);
+        shopButton.setOnClickListener(buttonListener);
+        mapButton.setOnClickListener(buttonListener);
+        statsButton.setOnClickListener(buttonListener);
+        mainButton.setOnClickListener(buttonListener);
     }
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Context context = StatsActivity.this;
+            // Figure out which button was pressed
+            switch (v.getId()) {
+                case R.id.b_home:
+                    startActivity(new Intent(context, HomeActivity.class));
+                    break;
+                case R.id.b_map:
+                    startActivity(new Intent(context, MapActivity.class));
+                    break;
+                case R.id.b_shop:
+                    startActivity(new Intent(context, ShopActivity.class));
+                    break;
+                case R.id.b_stats:
+                    break;
+                case R.id.b_main:
+                    startActivity(new Intent(context, MainActivity.class));
+                    break;
+            }
+        }
+    };
 }
