1deeaeefc2829bc7811c40bd0786e647e4a52858
melonlabe <lebal33@gmail.com>
2017-04-04 14:55:10 +0200

    Added Monster class
    Added Monster class

---
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/Monster.java
@@ -0,0 +1,43 @@
+package com.example.sauronsarmy.oopp;
+
+import android.media.Image;
+
+/**
+ * Created by Filip on 2017-04-04.
+ */
+
+public class Monster {
+
+    private int health;
+    private int gold;
+    private Image img;
+
+
+    public Monster(int health,int gold,Image img){
+
+        this.health = health;
+        this.gold = gold;
+        this.img = img;
+
+    }
+    //damage a monster, return true if dead
+    public boolean damageMonster(int damage){
+
+        this.health -= damage;
+        return this.health <= 0;
+
+    }
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public int getGold(){
+        return this.gold;
+    }
+
+    public Image getImg(){
+
+        return this.img;
+    }
+}
