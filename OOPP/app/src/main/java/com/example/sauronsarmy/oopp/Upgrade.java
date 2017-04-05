dd77446c807d4cd5c944813267765b1cdc728d3e
bunnyfiscuit <sarosh.nasir@gmail.com>
2017-04-05 15:58:52 +0200

    Home.java, Shop.java + Upgrade.java

---
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/Upgrade.java
@@ -0,0 +1,48 @@
+package com.example.sauronsarmy.oopp;
+
+/**
+ * Created by bunnyfiscuit on 05/04/17.
+ */
+
+class Upgrade {
+
+    private double baseStat;
+    private double currentStat;
+    private double baseCost;
+    private double currentCost;
+
+    protected Upgrade(double baseStat, double baseCost){
+        this.baseStat = this.currentStat = baseStat;
+        this.baseCost = this.currentCost = baseCost;
+
+    }
+
+    // Updates current stat upgrade amount with a multiplier
+    protected void updateStat(int multiplier){
+        currentStat *= multiplier;
+    }
+
+    // Updates the cost for the upgrade with multiplier
+    protected void updateCost(int multiplier){
+        if(multiplier > 1) {
+            currentCost *= (multiplier / 2);
+        }
+    }
+
+    protected double getBaseStat(){
+        return this.baseStat;
+    }
+
+    protected double getCurrentStat(){
+        return this.currentStat;
+    }
+
+    protected double getBaseCost(){
+        return this.baseCost;
+    }
+
+    protected double getCurrentCost(){
+        return this.currentCost;
+    }
+
+}
