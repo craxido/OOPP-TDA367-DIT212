ddb91c2e6ef124ea4c297d2cb5e3ed657a7d540c
Jonatan Källman <craxi@live.se>
2017-04-17 16:59:44 +0200

    Added MainPresenter and an interface containing interfaces used to communicate between the MVP layers.

---
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/IMainMVP.java
@@ -0,0 +1,32 @@
+package com.example.sauronsarmy.oopp;
+
+/**
+ * Created by Jonatan on 2017-04-17.
+ */
+
+/* "Umbrella" class containing the interfaces
+ * used to communicate between MVP layers.
+ */
+public interface IMainMVP {
+
+    /* Presenter -> View */
+    interface RequiredViewOps {
+        //methods to be called from View
+    }
+
+    /* View -> Presenter */
+    interface PresenterOps{
+        // methods to be called from View
+    }
+
+    /* Model -> Presenter */
+    interface RequiredPresenterOps {
+        // methods to be called from Model
+    }
+
+    /* Presenter -> Model */
+    interface ModelOps {
+        // model methods
+    }
+}
+
---
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/MainPresenter.java
@@ -0,0 +1,15 @@
+package com.example.sauronsarmy.oopp;
+
+/**
+ * Created by Jonatan on 2017-04-17.
+ */
+
+public class MainPresenter implements IMainMVP.RequiredPresenterOps, IMainMVP.PresenterOps {
+
+    //Model and View references
+
+    public MainPresenter(IMainMVP.RequiredViewOps mView) {
+
+    }
+
+}
