3254cf9abd658d32e052b9a261e2237e5553bf46
Jonatan Källman <craxi@live.se>
2017-04-25 12:20:35 +0200

    Added a basic Level Factory. Levels now contain one monster instead of an array of monsters. Makes sense since Areas have arrays of Levels.

---
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/levelFactory.java
@@ -0,0 +1,23 @@
+package com.example.sauronsarmy.oopp;
+
+/**
+ * Created by Jonatan on 2017-04-25.
+ */
+
+class levelFactory {
+    public levelFactory(){}
+    monsterFactory monfac = new monsterFactory();
+
+    public Level getLevel(areaType type){
+
+        // Level constructor:
+        // Level(Monster monster, double healthMultiplier, double goldMultiplier, areaType area)
+        switch(type){
+            case FOREST:return new Level(monfac.getMonster(100, 100, type), 1, 1, type);
+            case MOUNTAIN:return new Level(monfac.getMonster(200, 200, type), 1, 1, type);
+            case VOLCANO:return new Level(monfac.getMonster(300, 300, type), 1, 1, type);
+            default: return null;
+        }
+
+    }
+}
