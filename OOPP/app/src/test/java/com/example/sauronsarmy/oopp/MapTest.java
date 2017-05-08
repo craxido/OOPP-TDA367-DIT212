c7b118f7e0f6b721482480c7297af1d33d30911a
Jonatan Källman <craxi@live.se>
2017-05-08 22:41:13 +0200

    added 3 tests to MapTest.java and a getAreas method to Map.java

--- OOPP/app/src/test/java/com/example/sauronsarmy/oopp/MapTest.java
+++ OOPP/app/src/test/java/com/example/sauronsarmy/oopp/MapTest.java
@@ -6,6 +6,8 @@ package com.example.sauronsarmy.oopp;
 
 //Imports
 import com.example.sauronsarmy.oopp.Map.Map;
+import com.example.sauronsarmy.oopp.Map.areaType;
+import com.example.sauronsarmy.oopp.Map.Area;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
@@ -14,6 +16,7 @@ import org.junit.After;
 
 public class MapTest {
     private Map map;
+    Area[] areas = map.getAreas();
 
     @Before
     public void setUp() throws Exception {
@@ -25,5 +28,19 @@ public class MapTest {
         map = null;
     }
 
-
+    @Test
+    public void testCurrentArea() throws Exception {
+        map.setCurrentArea(areas[0]);
+        if(BuildConfig.DEBUG && map.getCurrentArea()==areas[0]){
+            throw new AssertionError();
+        }
+        map.setCurrentArea(areas[1]);
+        if(BuildConfig.DEBUG && map.getCurrentArea()==areas[1]){
+            throw new AssertionError();
+        }
+        map.setCurrentArea(areas[2]);
+        if(BuildConfig.DEBUG && map.getCurrentArea()==areas[2]){
+            throw new AssertionError();
+        }
+    }
 }
