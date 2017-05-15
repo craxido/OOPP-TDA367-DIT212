d968ee3268d373faf8b7784e91214fa2ac4d5075
Lebal <filip.labe123@gmail.com>
2017-05-15 17:07:01 +0200

    Fixed negative gold, bgref error

--- OOPP/app/src/main/java/com/example/sauronsarmy/oopp/MainPresenter.java
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/MainPresenter.java
@@ -73,7 +73,7 @@ import java.lang.ref.WeakReference;
     public void monsterClicked(){
         int gold;
         gold = mapPres.damageMonster(playerModel.getDamage());
-        if(gold != 0){
+        if(gold >0){
             playerModel.setMoney(playerModel.getMoney() + gold);
         }
     }
