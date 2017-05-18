9d90b492467e0cbcf3e4d086ff916838fff54fbf
Jonatan Källman <craxi@live.se>
2017-05-18 14:43:41 +0200

    Added a timer to BossMonster class and an update function which checks if a set time is reached.

--- OOPP/app/src/main/java/com/example/sauronsarmy/oopp/monsterPack/BossMonster.java
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/monsterPack/BossMonster.java
@@ -1,17 +1,32 @@
 package com.example.sauronsarmy.oopp.monsterPack;
 
+import com.example.sauronsarmy.oopp.clock.ClockListener;
+import com.example.sauronsarmy.oopp.clock.Runner;
+
 /**
  * @Author: Jonatan on 18/05/2017.
  */
 
-public class BossMonster extends Monster {
+public class BossMonster extends Monster implements ClockListener {
+
+    private Runner bossTimer;
+    private int time;
 
     public BossMonster(int health, int gold, int imageref){
         super(health,gold,imageref);
+        bossTimer=Runner.getInstance();
+        time=0;
     }
 
     private void resetHealth(){
         super.setHealth(super.getMaxHealth());
     }
 
+    @Override
+    public void update(){
+        time++;
+        if (time>30){
+            this.resetHealth();
+        }
+    }
 }
