7ee0c13df570bd818f3f4b3778c9dcd989a16277
Erik Sjöström <erikdsjostrom@gmail.com>
2017-05-24 18:59:23 +0200

    Updating tests and writing comments

--- OOPP/app/src/main/java/com/example/sauronsarmy/oopp/player/PlayerModel.java
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/player/PlayerModel.java
@@ -3,16 +3,15 @@ package com.example.sauronsarmy.oopp.player;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.example.sauronsarmy.oopp.MainActivity;
 import com.example.sauronsarmy.oopp.R;
 import com.example.sauronsarmy.oopp.clock.ClockListener;
 import com.example.sauronsarmy.oopp.clock.Runner;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.annotation.Inherited;
 
 /**
  * Created by Erik on 04/04/17.
+ * @author erik
  * Implemented the PlayerModel class using the singleton
  * pattern, since there should only be one instance
  * of a player.
@@ -27,7 +26,6 @@ public class PlayerModel implements PlayerModelInterface, ClockListener {
     private int damagePerSecond;
     private int money;
     private int moneyPerSecond;
-    private  long lastLogOn;
 
     /*
     Constructor is private since this implemented as a singelton.
@@ -42,41 +40,39 @@ public class PlayerModel implements PlayerModelInterface, ClockListener {
     public static PlayerModelInterface getInstance(Context context) {
         if (ourInstance == null)
             ourInstance = new PlayerModel(context);
-        return getInstance();
+        return ourInstance;
     }
 
     public static PlayerModelInterface getInstance() {
         return ourInstance;
     }
 
-    public long getLastLogOn() {
-        return lastLogOn;
+    /**
+     * {@inheritDoc}
+     * Sharedpreference is just a glorified key-value storage, the
+     * advantage being that it can be accessed from anywhere in the
+     * app, as long as there is a Context to use.
+     *
+     * Uses .apply() instead of .commit() which means that the state
+     * will be saved asynchronously. We avoid race conditions since
+     * the state will only be loaded once, when the model is created.
+     */
+    @Override
+    public void saveState(Context context) {
+        saveState = context.getSharedPreferences(context.getString(R.string.stateIdentifier),
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = saveState.edit();
+        editor.putInt("money", getMoney());
+        editor.putInt("moneyPerSec", getMoneyPerSecond());
+        editor.putInt("damage", getDamage());
+        editor.putInt("damagePerSec", getDamagePerSecond());
+        editor.apply();
     }
 
-    public void setLastLogOn(long lastLogOn) {
-        this.lastLogOn = lastLogOn;
-    }
-
-    public int getDamagePerSecond() {
-        return damagePerSecond;
-    }
-
-    public void setDamagePerSecond(int damagePerSecond) {
-        this.damagePerSecond = damagePerSecond;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public int getMoney() {
-        return money;
-    }
-
+    /**
+     * {@inheritDoc}
+     * Loads default values if no previous state has been saved.
+     */
     @Override
     public void loadState(Context context) {
         saveState = context.getSharedPreferences(context.getString(R.string.stateIdentifier),
@@ -86,40 +82,64 @@ public class PlayerModel implements PlayerModelInterface, ClockListener {
         setDamage(saveState.getInt("damage", 10));
         setDamagePerSecond(saveState.getInt("damagePerSec", 0));
     }
-
-    @Override
-    public void saveState(Context context) {
-        saveState = context.getSharedPreferences(context.getString(R.string.stateIdentifier),
-                                                 Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = saveState.edit();
-        editor.putInt("money", getMoney());
-        editor.putInt("moneyPerSec", getMoneyPerSecond());
-        editor.putInt("damage", getDamage());
-        editor.putInt("damagePerSec", getDamagePerSecond());
-        editor.apply();
-    }
-
-    private void setMoney(int money) {
-        this.money = money;
-    }
-
-    public void addMoney(int money) { this.money += money; }
-    public void removeMoney(int money) { this.money -= money; }
-
-    public int getMoneyPerSecond() {
-        return moneyPerSecond;
-    }
-
-    public void setMoneyPerSecond(int moneyPerSecond) {
-        this.moneyPerSecond = moneyPerSecond;
-    }
-
     /**
-     * Every second, update the money
+     * {@inheritDoc}
+     * Every second, update the money with the
+     * money earned per second
      */
     @Override
     public void update() {
         addMoney(moneyPerSecond);
+    }
+
+    /**
+     * Updates the state of the variable money
+     *      Only exists for consistency
+      * @param money New value
+     */
+    private void setMoney(int money) {
+        this.money = money;
+    }
+
+    @Override
+    public int getDamagePerSecond() {
+        return damagePerSecond;
+    }
+
+    @Override
+    public void setDamagePerSecond(int damagePerSecond) {
+        this.damagePerSecond = damagePerSecond;
+    }
+
+    @Override
+    public int getDamage() {
+        return damage;
+    }
+
+    @Override
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    public int getMoney() {
+        return money;
+    }
+
+    @Override
+    public void addMoney(int money) { this.money += money; }
+
+    @Override
+    public void removeMoney(int money) { this.money -= money; }
+
+    @Override
+    public int getMoneyPerSecond() {
+        return moneyPerSecond;
+    }
+
+    @Override
+    public void setMoneyPerSecond(int moneyPerSecond) {
+        this.moneyPerSecond = moneyPerSecond;
     }
 }
 
--- OOPP/app/src/main/java/com/example/sauronsarmy/oopp/player/PlayerModelInterface.java
+++ OOPP/app/src/main/java/com/example/sauronsarmy/oopp/player/PlayerModelInterface.java
@@ -4,26 +4,72 @@ import android.content.Context;
 
 /**
  * Created by Erik on 13/04/17.
+ * @author erik
  */
 
 public interface PlayerModelInterface {
 
-    void setLastLogOn(long newValue);
-    long getLastLogOn();
-
+    /**
+     * Updates the state of the variable damagePerSecond
+     * @param newValue The new value
+     */
     void setDamagePerSecond(int newValue);
+
+    /**
+     * @return The state of the variable damagePerSecond
+     */
     int getDamagePerSecond();
 
+    /**
+     * Updates the state of the variable damage
+     * @param newValue The new value
+     */
     void setDamage(int newValue);
+
+    /**
+     * @return The state of the variable damage
+     */
     int getDamage();
 
+    /**
+     * Removes an amount from the variable money
+     * @param money Amount to remove
+     */
     void removeMoney(int money);
+
+    /**
+     * Adds an amount to the variable money
+     * @param money Amount to add
+     */
     void addMoney(int money);
+
+    /**
+     * @return The state of the variable money
+     */
     int getMoney();
 
-    void loadState(Context context);
+    /**
+     * Sets the state of the variable moneyPerSecond
+     * @param moneyPerSecond The new value
+     */
+    void setMoneyPerSecond(int moneyPerSecond);
+
+    /**
+     * @return The state of the variable moneyPerSecond
+     */
+    int getMoneyPerSecond();
+
+    /**
+     * Saves the state of all the variables in the model to
+     * the internal storage.
+     * @param context Used to access the internal storage
+     */
     void saveState(Context context);
 
-    void setMoneyPerSecond(int newValue);
-    int getMoneyPerSecond();
+    /**
+     * Updates the state of the variables in the model with
+     * the state which is saved in the internal storage.
+     * @param context Used to access the internal storage
+     */
+    void loadState(Context context);
 }
