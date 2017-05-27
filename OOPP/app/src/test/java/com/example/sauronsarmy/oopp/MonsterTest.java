30fff6bcbb8fb5d72928d7a6833828db7ed5739c
Lebal <filip.labe123@gmail.com>
2017-05-27 20:34:40 +0200

    More tests

--- OOPP/app/src/test/java/com/example/sauronsarmy/oopp/MonsterTest.java
+++ OOPP/app/src/test/java/com/example/sauronsarmy/oopp/MonsterTest.java
@@ -12,11 +12,13 @@ package com.example.sauronsarmy.oopp;
 
 public class MonsterTest {
 
-    private Monster monster;
+    private Monster monster,monster2;
 
     @Before
     public void setUp() throws  Exception{
         monster = new Monster(10,10,R.drawable.mike, false);
+        monster2 = new Monster(10,10,R.drawable.mike,false);
+
 
     }
 
@@ -39,5 +41,42 @@ public class MonsterTest {
 
         assert (monster.getImageRef() == R.drawable.mike);
 
+    }
+
+    @Test
+    public void testEquals(){
+        assert (monster.equals(monster2));
+    }
+
+    @Test
+    public void isBossTest(){
+
+        assert (monster.isBoss() ==false);
+    }
+
+    @Test
+    public void getHealthTest(){
+
+        assert (monster.getHealth() == 10);
+
+    }
+
+    @Test
+    public void setHealthTest(){
+        monster.setHealth(15);
+        assert (monster.getHealth() ==15);
+
+    }
+
+    @Test
+    public void getGold(){
+
+        assert (monster.getGold()==10);
+    }
+
+    @Test
+    public void getMaxHealth(){
+
+        assert (monster.getMaxHealth() == 10);
     }
 }
